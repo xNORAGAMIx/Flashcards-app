bea98dbc1738fa676cb26b4496eef87155400231
Manas Das <noragami0021@gmail.com>
2025-07-19 22:58:28 +0530

    simple update

--- services/chat/socket/app.js
+++ services/chat/socket/app.js
@@ -2,22 +2,22 @@ import Message from "../models/Message.js";
 
 export const socketHandler = (io) => {
   io.on("connection", (socket) => {
-    console.log("New client connected");
-
     socket.on("joinGroup", (groupId) => {
       socket.join(groupId);
-      console.log(`Client joined group: ${groupId}`);
     });
 
-    socket.on("sendMessage", async ({ groupId, senderId, senderName, message }) => {
-      const msg = new Message({ groupId, senderId, senderName, message });
-      await msg.save();
+    socket.on(
+      "sendMessage",
+      async ({ groupId, senderId, senderName, message }) => {
+        const msg = new Message({ groupId, senderId, senderName, message });
+        await msg.save();
 
-      io.to(groupId).emit("newMessage", msg);
-    });
+        io.to(groupId).emit("newMessage", msg);
+      }
+    );
 
     socket.on("disconnect", () => {
-      console.log("Client disconnected");
+      console.log(" ");
     });
   });
-}
+};
